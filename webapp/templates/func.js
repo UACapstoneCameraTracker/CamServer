957e68cbbc13c7896f820129f07f47cff02955e8
harry996 <wenchen@ualberta.ca>
2020-03-15 14:39:11 -0600

    testing

--- webapp/templates/func.js
+++ webapp/templates/func.js
@@ -1,25 +1,25 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-c.addEventListener("click", function(evt) {
-    var mousePos = getMousePos(c, evt);
-    alert("push this to back end: " + mousePos.x + ',' + mousePos.y);
+// var c = document.getElementById("myCanvas");
+// var ctx = c.getContext("2d");
+// c.addEventListener("click", function(evt) {
+//     var mousePos = getMousePos(c, evt);
+//     alert("push this to back end: " + mousePos.x + ',' + mousePos.y);
 
-}, false);
+// }, false);
 
-//Get Mouse Position
-function getMousePos(c, evt) {
-    var rect = c.getBoundingClientRect();
-    return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
-    };
-}
+// //Get Mouse Position
+// function getMousePos(c, evt) {
+//     var rect = c.getBoundingClientRect();
+//     return {
+//         x: evt.clientX - rect.left,
+//         y: evt.clientY - rect.top
+//     };
+// }
 
-window.onload = function() {
-    var img = new Image();
+// window.onload = function() {
+//     var img = new Image();
 
-    img.onload = function() {
-        ctx.drawImage(img, 1280, 720);
-    }
-    img.src = "/home/pi/fifo_img.jpg";
-}
+//     img.onload = function() {
+//         ctx.drawImage(img, 1280, 720);
+//     }
+
+// }
