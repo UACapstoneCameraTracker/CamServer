1e907b9a1be2628e8eccf730ef85ef9974c6e6ac
harry996 <wenchen@ualberta.ca>
2020-03-25 19:57:11 -0600

    UNTESTED

--- webapp/app.py
+++ webapp/app.py
@@ -2,22 +2,22 @@ from flask import Flask, render_template, Response
 from camera import Camera
 from flask import request
 import json
-# from motor_control import gimbal
-
-# from motor_control import gimbal
+from motor_control import gimbal
+IMG_SIZE = (640, 360)
+FIFO_PATH_CMD = '/home/pi/fifo_cmd'
 
 
 
 app = Flask(__name__)
-# gimbal.init_gimbal((360,640))
+gimbal.init_gimbal(IMG_SIZE)
 camera = Camera()
 
 
 def streaming():
     global camera
     while True:
-        # frame = camera.get_frame()
-        frame = camera.get_frame_test()
+        frame = camera.get_frame()
+        # frame = camera.get_frame_test()
         if len(frame) > 0:
             
             yield (b'--frame\r\n'
@@ -29,11 +29,6 @@ def index():
     return render_template('index.html')
 
 
-# @app.route('/getmethod/<jsdata>')
-# def get_javascript_data(jsdata):
-#     return 12345
-#     # return json.loads(jsdata)
-#     # return jsdata
 
 
 @app.route('/postmethod', methods=['POST'])
@@ -45,8 +40,24 @@ def get_post_javascript_data():
     y1 = coorArray[1]
     x2 = coorArray[2]
     y2 = coorArray[3]
+    width = int(x2)-int(x1)
+    height = int(y2)-int(y1)
 
     print("x1: " + x1 + " y1: " + y1 + " x2: " + x2 + " y2:" + y2)
+    print("x1: " + x1 + " y1: " + y1 + " width: " + width + " height: " + height)
+
+    cmd = "select target"
+    bbox = cod1+","+cod2+","+lenth+","+width
+
+    with open(FIFO_PATH_CMD, 'w') as fifo:
+        fifo.flush()
+        fifo.write(cmd)
+        fifo.write('\n')
+        fifo.write(bbox)
+
+    fifo.close()
+
+
     return jsdata
 
 
@@ -55,7 +66,9 @@ def get_post_javascript_datal():
     jsdata = request.form['javascript_data']
     print("manual turning: ")
     print(jsdata)
+    
     print("call gimbal.move_to((-100,0)) or switch to pipe later ")
+    gimbal.move_to((IMG_SIZE[0]/4, IMG_SIZE[1]/2))
     
     return jsdata
 
@@ -65,6 +78,7 @@ def get_post_javascript_datar():
     print("manual turning: ")
     print(jsdata)
     print("call gimbal.move_to( .... ) or switch to pipe later ")
+    gimbal.move_to((IMG_SIZE[0]/4, IMG_SIZE[1]/2))
     
     return jsdata
 
@@ -75,6 +89,7 @@ def get_post_javascript_datau():
     print("manual turning: ")
     print(jsdata)
     print("call gimbal.move_to(.... ) or switch to pipe later ")
+    gimbal.move_to((IMG_SIZE[0]/4, IMG_SIZE[1]/2))
     
     return jsdata
 
@@ -85,15 +100,16 @@ def get_post_javascript_datad():
     print("manual turning: ")
     print(jsdata)
     print("call gimbal.move_to(.... ) or switch to pipe later ")
+    gimbal.move_to((IMG_SIZE[0]/4, IMG_SIZE[1]/2))
     
     return jsdata
 
 
 
-@app.route('/CruisingMode')
-def CruisingMode():
-    # send a signal to motorcontroller/otherthings to alert camera to change mode
-    return render_template('CruisingMode.html')
+# @app.route('/CruisingMode')
+# def CruisingMode():
+#     # send a signal to motorcontroller/otherthings to alert camera to change mode
+#     return render_template('CruisingMode.html')
 
 
 @app.route('/ManualMode')
@@ -102,49 +118,9 @@ def ManualMode():
     return render_template('ManualMode.html')
 
 
-@app.route('/turnleft')
-def turnleft():
-    # send a signal to motorcontroller/otherthings to alert camera to change mode
-    print("call motor to turn left ")
-    
-    
-    # gimbal.move_to((-100,0))
-    return render_template('ManualMode.html')
-    # return "nothing"
 
 
-@app.route('/turnright')
-def turnright():
-    # send a signal to motorcontroller/otherthings to alert camera to change mode
-    # gimbal.move_to((100,0))
-    print("call motor to turn right ")
-    return render_template('ManualMode.html')
 
-
-@app.route('/turnup')
-def turnup():
-    # send a signal to motorcontroller/otherthings to alert camera to change mode
-    # gimbal.move_to((0,100))
-    print("call motor to turn up ")
-    return render_template('ManualMode.html')
-
-
-@app.route('/turndown')
-def turndown():
-    # send a signal to motorcontroller/otherthings to alert camera to change mode
-    print("call motor to turn down ")
-    # gimbal.move_to((0,-100))
-    return render_template('ManualMode.html')
-    # return "nothing"
-
-
-@app.route('/stopturning')
-def stopturning():
-    # send a signal to motorcontroller/otherthings to alert camera to change mode
-    print("call motor to stop turning ")
-    # gimbal.init_gimbal((360,640))
-    return render_template('ManualMode.html')
-    # return "nothing"
 
 
 @app.route('/SelectTarget')
@@ -166,8 +142,7 @@ def video_feed():
 # multipart/x-mixed-replace content type must be used.
 
 
-# need to manually change the host ip address to pi's local ip address
-# so under the same wifi, ie, http://192.168.1.64:5000 to connect
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
     
